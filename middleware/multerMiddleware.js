f0fb9e1fcaba4d81bf1ab9547cd5a1b5099452df
SaranMuruganM <msaran913@gmail.com>
2024-06-29 20:26:41 +0530

    fixed profile

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -7,7 +7,8 @@ const upload = multer({ storage });
 const parser = new DataParser();
 
 export const formatImage = (file) => {
-  const fileExtension = path.extname(file.orginalname).toString();
+  const fileExtension = path.extname(file.originalname).toString();
+  console.log(fileExtension);
   return parser.format(fileExtension, file.buffer).content;
 };
 
